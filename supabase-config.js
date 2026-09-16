39b5d34cfbcac6bfbe1a1fc7f5e974c4a8d0e1df
Kevin Beltran <44617053+KBeltranJ@users.noreply.github.com>
2026-09-15 22:36:59 -0400

    Add Audrey-only Supabase public client config

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+window.AUDREY_SUPABASE = Object.freeze({
+  url: "https://xepcruqasersecblorjn.supabase.co",
+  publishableKey: "sb_publishable_EjteBoFD3Mjn38VUC18NiA_bMvXHBqu",
+  projectRef: "xepcruqasersecblorjn",
+  resumeBucket: "audrey-resumes"
+});
